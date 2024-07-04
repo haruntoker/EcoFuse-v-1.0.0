1f424c20b979f264143ac14bdd0ef5f8b4484baf
harun toker <haruntoker@icloud.com>
2024-07-04 22:18:21 +0300

    dark mode positioned.

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -2,7 +2,7 @@ import {  DarkThemeToggle } from 'flowbite-react';
 
 export default function DarkMode() {
   return (
-    <div className="flex top-4 right-4">
+    <div className="flex top-4 right-4 bottom-8">
     <DarkThemeToggle />
   </div>
   )
